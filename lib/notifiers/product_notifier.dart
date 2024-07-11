e0f34130f02d3e5223ddf2e0e3907a676df65e1c
Beejay9 <omobolajibaruwa@gmail.com>
2024-07-11 09:13:27 +0100

    uppp

--- lib/notifiers/product_notifier.dart
+++ lib/notifiers/product_notifier.dart
@@ -11,9 +11,9 @@ class ProductRepository extends ChangeNotifier {
   Future<void> fetchProduct() async {
     try {
       isLoading = true;
-      const apiKey = '68b414f58e8b488885a93b29eb78ff9320240710151345626512';
-      const apiId = 'CGL0MHNKC6994BN';
-      const organizationId = '7d9356c8bd3f4b3c9c47fab30da9ef8f';
+      const apiKey = '*********************************';
+      const apiId = '******************';
+      const organizationId = '******************';
       final url = Uri.parse(
         'https://api.timbu.cloud/products?organization_id=$organizationId&reverse_sort=false&page=1&size=10&Appid=$apiId&Apikey=$apiKey',
       );
